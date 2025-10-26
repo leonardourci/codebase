72316f908dd30af5ded9a9d398ce25f42549ee8a
Leonardo Guimarães Urci <leonardoguimaraesurci.com>
2025-10-26 13:00:10 -0300

    chore: swagger setup

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -1,15 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setVersion('1.0')
+    .setTitle('Define Your API Title')
+    .setDescription('API description goes here')
+    .addTag('Define your tag here')
+    .build();
+  const documentFactory = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/api/docs', app, documentFactory);
+
   app.useGlobalPipes(
     new ValidationPipe({
+      // Automatically transform payloads to be objects typed according to their DTO classes
       transform: true,
+
+      // Remove properties not defined in the DTOs
       whitelist: true,
     }),
   );
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
