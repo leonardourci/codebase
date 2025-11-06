54a15b9ebadae29c6e4d7a87134a493b28b6192b
Leonardo Guimarães Urci <leonardoguimaraesurci.com>
2025-11-06 19:12:39 -0300

    jwt import, export

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { AuthController } from './auth/auth.controller';
+import { AppConfigModule } from './app-config/app-config.module';
+import { AuthModule } from './auth/auth.module';
+import { JwtStrategy } from './auth/jwt.strategy';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AppConfigModule, UsersModule, AuthModule],
+  controllers: [AuthController],
+  exports: [JwtStrategy],
+  providers: [AppService, JwtStrategy],
 })
 export class AppModule {}
